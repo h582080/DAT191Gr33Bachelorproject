e67297216eaea29453f12492480c16ff2cadf7d6
stino <stino@DESKTOP-N6A5OIT>
2023-03-28 03:19:53 +0200

    First commit

---
+++ DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/Dat191Gr33BachelorprojectApplication.java
@@ -0,0 +1,13 @@
+package hvl.dat191;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat191Gr33BachelorprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat191Gr33BachelorprojectApplication.class, args);
+	}
+
+}
---
+++ DAT191Gr33Bachelorproject/src/test/java/hvl/dat1/Dat191Gr33BachelorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.dat1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat191Gr33BachelorprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
