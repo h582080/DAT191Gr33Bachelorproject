5b7aaf1d0e6599038633fadbcfa498dd3d193ee4
Even <582621@stud.hvl.no>
2023-04-21 15:52:10 +0200

    Database fungerer for internal

--- DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/Dat191Gr33BachelorprojectApplication.java
+++ DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/Dat191Gr33BachelorprojectApplication.java
@@ -3,7 +3,7 @@ package hvl.dat191;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="hvl.dat191")
 public class Dat191Gr33BachelorprojectApplication {
 
 	public static void main(String[] args) {
---
+++ DAT191Gr33Bachelorproject/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert  into internal
+(
+internalId,
+name,
+phoneNr,
+email
+)
+values
+(
+1,
+'Test Test',
+12345678,
+'testmail@test.com'
+);
