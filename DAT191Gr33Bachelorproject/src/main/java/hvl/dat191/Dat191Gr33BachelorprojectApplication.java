96da8c97f6a52109d35f005b928d7a0c12086f5d
stino <stino@DESKTOP-N6A5OIT>
2023-04-28 13:54:31 +0200

    Flyttet på filer
    Lagt til test form
    createprojecttcontroller test lagt til

--- DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/Dat191Gr33BachelorprojectApplication.java
+++ DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/Dat191Gr33BachelorprojectApplication.java
@@ -1,13 +1,17 @@
 package hvl.dat191;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication(scanBasePackages="hvl.dat191")
 public class Dat191Gr33BachelorprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dat191Gr33BachelorprojectApplication.class, args);
+				
 	}
 
 }
--- DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/controller/CreateProjectControllerTEST.java
+++ DAT191Gr33Bachelorproject/src/main/java/hvl/dat191/controller/CreateProjectControllerTEST.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import hvl.dat191.model.Projectdescription;
 import hvl.dat191.service.ProjectdescriptionService;
@@ -15,50 +14,19 @@ import hvl.dat191.service.ProjectdescriptionService;
 @RequestMapping("/createprojecttest")
 public class CreateProjectControllerTEST {
 	
+	private ProjectdescriptionService pdsi;
 	
-    private ProjectdescriptionService pdsi;
-	
-    @GetMapping("/projectdescription")
+	@GetMapping("/projectdescription") 
     public String createProjectForm(Model model) {
     	model.addAttribute("projectdescription", new Projectdescription());
-    	return("projectdescription");
+    	return "projectdescription";
     }
     
     @PostMapping("/projectdescription")
-    public String createProjectSubmit(
-    		@ModelAttribute Projectdescription projectdescription, Model model)	{
-    	model.addAttribute("projectdescription", projectdescription);
-    	
-    	pdsi.insertProjectdescription(projectdescription);
+    public String createProjectSubmit(@ModelAttribute Projectdescription projectdescription, Model model)	{
+    	model.addAttribute("projectdescription", projectdescription);   	
     	return "resulttest";
     }
     
-    
-  //  
-//	@PostMapping("/createProject2")
-//			@RequestParam String companyInfo, @RequestParam String projectDescription, 
-    //			@RequestParam String tools, @RequestParam String language, 
-    //			@RequestParam String programminglanguage, @RequestParam String projectType,
-    //			@RequestParam String theme, @RequestParam String webAddress, 
-    //			@RequestParam String contactPerson, @RequestParam String semester, 
-    //			@RequestParam String dateOfApproval ) {
-				//		
-    //		Projectdescription projectdescription = new Projectdescription(
-    //				projectTitel, 
-				//				companyInfo,
-    //				projectDescription,
-				//				tools,
-    //				language,
-    //				programminglanguage,
-    //				projectType,
-    //				theme,
-    //				webAddress,
-    //				contactPerson,
-    //				"WAITING",
-    //				semester,
-    //				dateOfApproval);
-    //		
-    //		pdsi.insertProjectdescription(projectdescription);
-		//	}
 
 }
